032b7e659b81e39ad35409bc35e75909c860a308
slabwu <134193743+slabwu@users.noreply.github.com>
2024-11-23 21:54:06 +0800

    Add getBoard() & playMove() methods

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const board = (function () {
     for (let i = 0; i < rows; i++) {
         values[i] = [];
         for (let j = 0; j < columns; j++) {
-            values[i].push("X");
+            values[i].push("");
         }
     }
 
-    return { values };
+    const getBoard = () => values;
+
+    const playMove = (square,player) => {
+        let row = Math.floor(square/columns);
+        let column = square % columns;
+        if (!values[row][column]) values[row][column] = player;
+        console.log({square, row, column});
+        //console.log(board.getBoard());
+    }
+
+    return { getBoard, playMove };
 })();
