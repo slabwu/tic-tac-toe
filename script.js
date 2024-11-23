7cd6be9e5eaab2015a363afad8a64875c0c8f3ca
slabwu <134193743+slabwu@users.noreply.github.com>
2024-11-23 22:14:36 +0800

    Add printBoard() method

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const board = (function () {
     for (let i = 0; i < rows; i++) {
         values[i] = [];
         for (let j = 0; j < columns; j++) {
-            values[i].push("");
+            values[i].push("_");
         }
     }
 
@@ -15,10 +15,20 @@ const board = (function () {
     const playMove = (square,player) => {
         let row = Math.floor(square/columns);
         let column = square % columns;
-        if (!values[row][column]) values[row][column] = player;
-        console.log({square, row, column});
-        //console.log(board.getBoard());
+        if (values[row][column] === "_") values[row][column] = player;
+        board.printBoard();
     }
 
-    return { getBoard, playMove };
+    const printBoard = () => {
+        let string = "";
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                string += values[i][j] + " ";
+                if (j === 2) string += "\n";
+            }
+        }
+        console.log(string)
+    };
+
+    return { getBoard, playMove, printBoard };
 })();
